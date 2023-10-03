3adf6a4aecdae2b60990005e93e04c9036b12ff0
Tatianna <tatirobinson@gmail.com>
2023-10-03 16:13:48 -0700

    Add slideable module

---
+++ slideable.rb
@@ -0,0 +1,34 @@
+module Slideable
+
+
+    def horizontal_dirs
+
+    end
+
+    def diagnal_dirs
+
+    end
+
+    def moves
+
+
+    end
+
+
+
+    private
+
+    @HORIZONTAL_DIRS = []
+    @DIAGNAL_DIRS = []
+
+    def move_dirs
+
+    end
+
+    
+    def grow_unblocked_moves_in_dir(dx,dy)
+
+    end
+
+
+end
