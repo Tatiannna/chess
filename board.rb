6e793f1c7cdc60d8c230019c5c12b6a890602a1b
Tatianna <tatirobinson@gmail.com>
2023-10-03 14:33:09 -0700

    initial board commit

---
+++ board.rb
@@ -0,0 +1,76 @@
+#require_relative "piece.rb"
+
+class Board
+
+    def initialize
+        @rows = Array.new(8){Array.new(8)}
+        #@null_piece = NullPiece
+    end
+
+
+    def [](pos)
+        row, col = pos
+        @rows[row][col]
+    end
+
+
+    def []=(pos, val)
+        row, col = pos
+        @rows[row][col] = val
+    end
+
+
+    def move_piece(start_pos, end_pos)
+        # will need color as a parameter but excluded it for now, just for testing :)
+        
+    end
+
+
+    def valid_pos?(pos)
+
+    end
+
+
+    def add_piece(piece, pos)
+
+    end
+
+
+
+    def checkmate?(color)
+
+    end
+
+
+
+    def in_check?(color)
+
+    end
+
+
+
+    def find_king(color)
+
+    end
+
+
+
+    def pieces
+
+    end
+
+
+
+    def dup
+
+    end
+
+
+
+    def move_piece!(color, start_pos, end_pos)
+
+    end
+
+
+
+end
