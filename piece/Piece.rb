6be35e562a79af91154f6a5e31d1eae0bbd2b9dd
DanniZ <denise13070823@gmail.com>
2023-10-04 15:42:48 -0700

    update pawn & piece

--- piece/Piece.rb
+++ piece/Piece.rb
@@ -31,8 +31,6 @@ class Piece
 
   end
 
-  def empty?
-    false
-  end
+
 end
 
--- piece/pawn.rb
+++ piece/pawn.rb
@@ -3,20 +3,51 @@ require_relative "piece.rb"
 
 class Pawn < Piece
 
+    def symbol
+        color? == :black? "♟" : "♙"
+    end
+
+
+    def moves
+        moves = forward_dir + side_attacks
+    end    
+
     private
 
     def at_start_row?
-        row < col = @pos
-        row == 1 || row == 6
+        color == :black? && current_row == 1 ||
+        color == :white? && current_row == 6
     end
 
     def forward_dir
+        color == :black? 1 : -1
     end
 
     def forward_steps
+        moves = []
+        current_row,current_col = pos
+        one_forward = []
+        if board[one_forward].empty?
+            moves << one_forward
+        end
+        two_forward = [current_row + (forward_dir * 2), current_col]
+        if board[two_forward].empty?
+            moves << two_forward
+        end
+        moves
     end
 
     def side_attacks
+        moves = []
+        check_left  = [current_row + forward_dir,current_col + 1]
+        check_right = [current_row + forward_dir,current_col - 1]
+        if board[check_left].color != board[pos].color
+            moves << check_left
+        end
+        if board[check_right].color != board[pos].color
+            moves << check_right
+        end
+        moves
     end
 
 end
