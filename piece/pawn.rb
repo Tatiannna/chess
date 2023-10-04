9a911ba5a9ae59cfde63ebe4ef1bc68fba4bc142
Tatianna <tatirobinson@gmail.com>
2023-10-04 09:57:47 -0700

    add methods to pawn class

--- piece/pawn.rb
+++ piece/pawn.rb
@@ -3,6 +3,20 @@ require_relative "piece.rb"
 
 class Pawn < Piece
 
+    private
 
+    def at_start_row?
+        row < col = @pos
+        row == 1 || row == 6
+    end
+
+    def forward_dir
+    end
+
+    def forward_steps
+    end
+
+    def side_attacks
+    end
 
 end
