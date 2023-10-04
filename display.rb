eec9eb53a4eda1b31c71873e3a31e77075088407
DanniZ <denise13070823@gmail.com>
2023-10-04 12:16:37 -0700

    feat: add display

---
+++ display.rb
@@ -0,0 +1,24 @@
+require_relative "board.rb"
+
+class Display
+  attr_reader :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def render
+    8.times do |row|
+      puts "-----------------------"
+      8.times do |col|
+        piece = board[[row,col]]
+        if piece = nil?
+          print " "
+        else
+          print "#{board[[row,col]]}"
+      end
+    end
+    puts ""
+  end
+end
+end
