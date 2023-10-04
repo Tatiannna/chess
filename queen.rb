7b9b55bf781bd74e82848ce6812a188c1c33571c
Tatianna <tatirobinson@gmail.com>
2023-10-03 17:47:42 -0700

    add to bishop, queen, and slideable

--- queen.rb
+++ queen.rb
@@ -4,7 +4,25 @@ require_relative "slideable.rb"
 class Queen < Piece
     include Slideable
 
+    def move_dirs
+        new_positions = []
+        # @DIAGNAL_DIRS = [[1,1], [-1,1], [-1, -1], [1, -1]]
+        # @HORIZONTAL_DIRS = [ [0, 1], [0, -1], [1, 0], [-1, 0]] # includes vertical!
 
+        row, col = @pos 
 
+        @DIAGNAL_DIRS.each do |delta|
+            dx, dy = delta
+            new_positions << row + delta, col + delta
+        end
 
+        @HORIZONTAL_DIRS.each do |delta|
+            dx, dy = delta
+            new_positions << row + delta, col + delta
+        end
+
+        new_positions
+    end
+
+    
 end
